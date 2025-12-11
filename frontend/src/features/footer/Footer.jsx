6f2de90846b3ef43f7cbc632c926937e9fab113a
joelmenacho <joelmenacho@gmail.com>
2025-12-10 19:47:37 -0500

    prueba final

--- frontend/src/features/footer/Footer.jsx
+++ frontend/src/features/footer/Footer.jsx
@@ -88,7 +88,7 @@ export const Footer = () => {
 
             {/* lower */}
             <Stack alignSelf={"center"}>
-                <Typography color={'GrayText'}>&copy; Mern Store {new Date().getFullYear()}. All right reserved</Typography>
+                <Typography color={'GrayText'}>&copy; Menacho Store {new Date().getFullYear()}. All right reserved</Typography>
             </Stack>
 
     </Stack>
